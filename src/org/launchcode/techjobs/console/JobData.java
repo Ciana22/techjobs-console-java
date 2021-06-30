286e13474e3f81cc17f428ff18aba0e6b4a05e9b
Laciana Hughes <laciana_hughes@cable.comcast.com>
2021-06-30 18:12:38 -0400

    fin

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by LaunchCode
@@ -57,27 +58,47 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
-     *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
-     * @return List of all jobs matching the criteria
+     * <p>
+     * //     * @param column   Column that should be searched.
+     * //     * @param value Value of teh field to search for
+     * //     * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
-
+//first line declaring the method; the arraylist is being returned
         // load data, if not already loaded
         loadData();
-
+//loadata loads the alljobs data
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        value = value.toLowerCase();
 
         for (HashMap<String, String> row : allJobs) {
 
             String aValue = row.get(column);
+            aValue = aValue.toLowerCase();
 
+            //System.out.println(aValue.toLowerCase());
             if (aValue.contains(value)) {
                 jobs.add(row);
+            }
+        }
+
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String searchValue) {
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> job : allJobs) {
+
+            for (String value : job.values()) {
+                if (value.toLowerCase().contains(searchValue.toLowerCase())) {
+                    jobs.add(job);
+                    break;
+                }
             }
         }
 
@@ -93,6 +114,7 @@ public class JobData {
         if (isDataLoaded) {
             return;
         }
+
 
         try {
 
